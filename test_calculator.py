9665e750fdd5411becf8b2cb125901b57b336394
PrincessPiggy22 <mlpuryear@my.waketech.edu>
2026-04-29 12:10:08 +0000

    Floor Divide test implementation done

--- test_calculator.py
+++ test_calculator.py
@@ -1,38 +1,47 @@
 import pytest
-from calculator import Calculator
+from Calculator import Calculator
 @pytest.fixture
 def calc():
     return Calculator()
 
-class TestAdd:
+#class TestAdd:
 
-    def test_positive_overflow(self, calc):
-        with pytest.raises(OverflowError):
-            calc.divide(1, 9223372036854775807)
+   # def test_positive_overflow(self, calc):
+        #with pytest.raises(OverflowError):
+       #     calc.divide(1, 9223372036854775807)
 
-class TestSubtract:
-
-
-class TestMultiply:
+#class TestSubtract:
 
 
-class TestDivide:
-
-    def test_even_division(self, calc):
-        assert calc.divide(10, 2) == pytest.approx(5.0)
-
-    def test_zero_divisor_raises(self, calc):
-        with pytest.raises(ZeroDivisionError):
-            calc.divide(5, 0)
+#class TestMultiply:
 
 
-class TestPower:
+#class TestDivide:
+
+#    def test_even_division(self, calc):
+#        assert calc.divide(10, 2) == pytest.approx(5.0)
+
+#    def test_zero_divisor_raises(self, calc):
+#        with pytest.raises(ZeroDivisionError):
+#            calc.divide(5, 0)
 
 
-class TestSquareRoot:
+#class TestPower:
 
 
-class TestModulus:
+#class TestSquareRoot:
+
+
+#class TestModulus:
 
 
 class TestFloorDivide:
+    def test_raises_zero_division(self,calc):
+        with pytest.raises(ZeroDivisionError):
+           Calculator.floor_divide(5,0)
+
+    def test_dividing_boundary(self,calc):
+        assert Calculator.floor_divide(9223372036854775807,30.0) == pytest.approx(3.0744573e+17)
+
+    def test_floor_division(self,calc):
+        assert Calculator.floor_divide(30,5) == 6
